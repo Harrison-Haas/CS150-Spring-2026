97db801bd4c26e8bdcd14764986c8a81f1f1fb72
Harrison Haas <harrisonhaas3@gmail.com>
2026-04-03 15:24:43 -0500

    Adding Assignment03

---
+++ Assignment03/src/lab2/Rectangle.java
@@ -0,0 +1,21 @@
+package lab2;
+
+import java.awt.Graphics;
+
+public class Rectangle extends Shape {
+    
+    private int width;
+    private int height;
+
+    public Rectangle(int x, int y, int width, int height){
+        super(x, y);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void draw (Graphics g){
+        g.drawRect(this.getX(), this.getY(), this.width, this.height);
+    }
+    
+}
